74cfde77a198877e7816031dd1538fb0778e866c
Pablo Luan <pablo-544@hotmail.com>
2024-12-08 17:48:26 -0300

    feat: Updating reports page and create of statitreport.ts

--- web/src/components/card-report.tsx
+++ web/src/components/card-report.tsx
@@ -1,20 +1,28 @@
+import { staticReportsProps } from "@/lib/staticreports";
 import { MapPinPlusInside } from "lucide-react";
 
-export default function ReportCard(){
+interface ReportProps {
+    data: staticReportsProps[]
+}
+
+export default function ReportCard({data}: ReportProps){
+    
     return (
-    <div className="flex flex-col">
-        <div className="bg-custom-primary text-white mx-3 py-3 px-5 rounded-xl flex flex-col gap-1">
-            <div className="flex items-center gap-3">
-                <MapPinPlusInside width={30} height={37}/>
-                <div className="flex flex-col gap-[0.125rem]">
-                    <h2 className="text-lg leading-none">+1 Ta Barril</h2>
-                    <h3 className="text-sm">Queimadinha</h3>
+    <div className="flex flex-col gap-6">
+        {data.map((report, index) => (
+            <div key={index + 1} className="bg-custom-primary text-white mx-3 py-3 px-5 rounded-xl flex flex-col gap-1">
+                <div className="flex items-center gap-3">
+                    <MapPinPlusInside width={30} height={37}/>
+                    <div className="flex flex-col gap-[0.125rem]">
+                        <h2 className="text-lg leading-none">{report.title}</h2>
+                        <h3 className="text-sm">{report.location}</h3>
+                    </div>
                 </div>
+                <p className="text-base line-clamp-3">
+                    {report.content}
+                </p>
             </div>
-            <p className="text-base line-clamp-3">
-                Um menino bom me abordou, por volta das 15h com uma faca e tive que passar meu celular, tava paga.
-            </p>
-        </div>
+        ))}
     </div>
     )
 }
--- web/src/lib/staticroutes.ts
+++ web/src/lib/staticroutes.ts
@@ -159,3 +159,5 @@ export const policeStation: StaticRoutes[] = [
         }
     },
 ]
+
+
