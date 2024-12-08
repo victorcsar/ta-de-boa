0ed17d163b77ea505ef8b6aca821de925753dbbd
Antônio Neto <acm.neto1999@gmail.com>
2024-12-08 15:06:54 -0300

    fix: adjusting minor details from settings page

--- web/src/app/(pages)/settings/page.tsx
+++ web/src/app/(pages)/settings/page.tsx
@@ -1,11 +1,11 @@
-import { User, UserCircle2 } from "lucide-react";
+import { UserCircleIcon } from "lucide-react";
 
 export default function Settings() {
   return (
     <div className="flex flex-col items-center h-full p-20 bg-gray-100 gap-20">
       <h2 className="text-2xl font-bold mb-8">CONFIGURAÇÃO</h2>
       <div className="flex flex-col items-center mb-8">
-        <UserCircle2 size={120} />
+        <UserCircleIcon size={150} />
         <p className="text-xl font-semibold">Fulano de Tal</p>
       </div>
       <div className="flex flex-col space-y-6 w-full">
@@ -15,7 +15,7 @@ export default function Settings() {
         <button className="bg-custom-primary text-white uppercase py-6 w-full rounded-2xl">
           Editar conta
         </button>
-        <button className="bg-custom-primary text-white uppercase py-6 w-full rounded-2xl">
+        <button className="bg-red-600 text-white uppercase py-6 w-full rounded-2xl">
           Logout
         </button>
       </div>
