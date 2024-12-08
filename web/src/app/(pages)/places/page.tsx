17e0cc9c96a4fe1781abe704465d7d9211bc6253
Pablo Luan <pablo-544@hotmail.com>
2024-12-08 15:20:04 -0300

    feat: Create of places page component

--- web/src/app/(pages)/places/page.tsx
+++ web/src/app/(pages)/places/page.tsx
@@ -1,6 +1,15 @@
-export default function Place() {
-    return (
-    <div>Places pages
+import GoogleMap from "@/components/google_features/map";
+import MapsAndPins from "@/components/google_features/markersandpins";
+import { policeStation } from "@/lib/staticroutes";
 
-    </div>)
+export default function Place() {
+    const data = policeStation
+    return (
+        <div className="flex flex-col w-full h-screen mb-9">    
+          <GoogleMap>
+            <MapsAndPins data={data} />
+          </GoogleMap>
+    
+        </div>
+      );
 }
