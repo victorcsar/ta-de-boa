92a406a8d116ebece2e3661ec8e804342d18ea70
Antônio Neto <acm.neto1999@gmail.com>
2024-12-08 15:37:48 -0300

    feat: implement CustomButton component and update settings page

--- web/src/app/(pages)/settings/page.tsx
+++ web/src/app/(pages)/settings/page.tsx
@@ -1,23 +1,20 @@
+import CustomButton from "@/components/button-custom";
 import { UserCircleIcon } from "lucide-react";
 
 export default function Settings() {
   return (
     <div className="flex flex-col items-center h-full p-20 bg-gray-100 gap-20">
-      <h2 className="text-2xl font-bold mb-8">CONFIGURAÇÃO</h2>
+      <h2 className="text-custom-primary text-2xl font-bold mb-8">
+        CONFIGURAÇÃO
+      </h2>
       <div className="flex flex-col items-center mb-8">
         <UserCircleIcon size={150} />
         <p className="text-xl font-semibold">Fulano de Tal</p>
       </div>
       <div className="flex flex-col space-y-6 w-full">
-        <button className="bg-custom-primary text-white uppercase py-6 w-full rounded-2xl">
-          Tutorial
-        </button>
-        <button className="bg-custom-primary text-white uppercase py-6 w-full rounded-2xl">
-          Editar conta
-        </button>
-        <button className="bg-red-600 text-white uppercase py-6 w-full rounded-2xl">
-          Logout
-        </button>
+        <CustomButton label="Tutorial" color="bg-custom-primary" />
+        <CustomButton label="Editar conta" color="bg-custom-primary" />
+        <CustomButton label="Logout" color="bg-red-600" />
       </div>
     </div>
   );
