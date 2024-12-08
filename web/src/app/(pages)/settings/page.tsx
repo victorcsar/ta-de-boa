d38fb2d48a01256db9ba05492ff29a27228fa10a
Antônio Neto <acm.neto1999@gmail.com>
2024-12-08 15:54:57 -0300

    refactor: adjusting layout of settings page

--- web/src/app/(pages)/settings/page.tsx
+++ web/src/app/(pages)/settings/page.tsx
@@ -3,15 +3,15 @@ import { UserCircleIcon } from "lucide-react";
 
 export default function Settings() {
   return (
-    <div className="flex flex-col items-center h-full p-20 bg-gray-100 gap-20">
-      <h2 className="text-custom-primary text-2xl font-bold mb-8">
+    <div className="flex flex-col items-center h-full">
+      <h2 className="text-custom-primary text-2xl font-bold pt-20 pb-10">
         CONFIGURAÇÃO
       </h2>
-      <div className="flex flex-col items-center mb-8">
+      <div className="flex flex-col items-center py-10">
         <UserCircleIcon size={150} />
         <p className="text-xl font-semibold">Fulano de Tal</p>
       </div>
-      <div className="flex flex-col space-y-6 w-full">
+      <div className="flex flex-col space-y-6 w-full pt-10 px-20">
         <CustomButton label="Tutorial" color="bg-custom-primary" />
         <CustomButton label="Editar conta" color="bg-custom-primary" />
         <CustomButton label="Logout" color="bg-red-600" />
