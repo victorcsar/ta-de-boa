03571ec8d87ec6eaa19b5be433e80ce73ec55bdd
Antônio Neto <acm.neto1999@gmail.com>
2024-12-08 17:09:16 -0300

    fix: fixing components padding

--- web/src/app/(pages)/settings/page.tsx
+++ web/src/app/(pages)/settings/page.tsx
@@ -8,10 +8,10 @@ export default function Settings() {
         CONFIGURAÇÃO
       </h2>
       <div className="flex flex-col items-center py-10">
-        <UserCircleIcon size={150} />
+        <UserCircleIcon size={175} />
         <p className="text-xl font-semibold">Fulano de Tal</p>
       </div>
-      <div className="flex flex-col space-y-6 w-full pt-10 px-20">
+      <div className="flex flex-col space-y-6 w-full pt-10 pb-40 px-20">
         <CustomButton label="Tutorial" color="bg-custom-primary" />
         <CustomButton label="Editar conta" color="bg-custom-primary" />
         <CustomButton label="Logout" color="bg-red-600" />
